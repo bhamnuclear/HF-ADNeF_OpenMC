e615874456ef30c987cba2fefa6c4dadd624c1d4
Max Conroy <mjc970@student.bham.ac.uk>
2024-01-12 15:55:09 +0000

    added 2.6 MeV file source

---
+++ fuente_LiMetal_Epmin_0_Epmax_2.60/build/CMakeFiles/source.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source.dir/Li_source_Epmin_0_Epmax_2.60.cpp.o"
+  "CMakeFiles/source.dir/Li_source_Epmin_0_Epmax_2.60.cpp.o.d"
+  "libsource.pdb"
+  "libsource.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
